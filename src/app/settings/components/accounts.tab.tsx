904692c1ea882ed0ef7f973a2c6563a46602ce3b
jonathanduran7 <joniduran624@gmail.com>
2024-01-12 22:19:42 -0300

    Add account dialog and functionality

--- src/app/settings/components/accounts.tab.tsx
+++ src/app/settings/components/accounts.tab.tsx
@@ -1,5 +1,8 @@
 'use client'
 
+import { Button, Dialog, DialogBody, DialogFooter, DialogHeader } from "@material-tailwind/react";
+import { useState } from "react";
+
 interface Account {
     id: number;
     name: string;
@@ -25,6 +28,11 @@ const data: Account[] = [
 ]
 
 export default function AccountTab() {
+
+    const [open, setOpen] = useState(false);
+
+    const handleOpen = () => setOpen(!open);
+
     return (
         <div>
             <div className="mb-8">
@@ -32,6 +40,12 @@ export default function AccountTab() {
                     Accounts are used to track your money.
                     You can have as many accounts as you want.
                 </p>
+            </div>
+
+            <div className="flex justify-end mb-3">
+                <button className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded" onClick={handleOpen}>
+                    Add account
+                </button>
             </div>
 
             <div>
@@ -51,6 +65,31 @@ export default function AccountTab() {
                     })
                 }
             </div>
+
+            <Dialog open={open} handler={handleOpen} placeholder={""}>
+                <DialogHeader placeholder={""}>Agregar nueva cuenta</DialogHeader>
+                <DialogBody placeholder={""}>
+                    <div className="flex flex-col">
+                        <label className="text-gray-600">Name</label>
+                        <input type="text" className="border border-gray-300 rounded-sm px-4 py-2" />
+                    </div>
+
+                    <div className="flex flex-col mt-4">
+                        <label className="text-gray-600">Currency</label>
+                        <select className="border border-gray-300 rounded-sm px-4 py-2">
+                            <option value="ARS">ARS</option>
+                            <option value="USD">USD</option>
+                        </select>
+                    </div>
+                </DialogBody>
+                <DialogFooter placeholder={""}>
+                    <div className="flex gap-3">
+                        <button className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded" onClick={handleOpen}>Save</button>
+
+                        <button className="text-gray-600 hover:text-gray-900" onClick={handleOpen}>Cancel</button>
+                    </div>
+                </DialogFooter>
+            </Dialog>
         </div>
     )
 }
