cb70be285d66f4097a61037820badd5eeb5544ef
jonathanduran7 <joniduran624@gmail.com>
2024-01-12 21:27:14 -0300

    implement redirections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,26 @@
+'use client'
+import { useRouter } from "next/navigation"
+
 export default function Home() {
+
+  const router = useRouter()
+
+  const goToPage = (page: 'settings' | 'movements') => {
+    router.push(`/${page}`)
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="flex gap-4">
-        <div className="border-2 border-black w-40 h-40 flex justify-center items-center rounded-lg cursor-pointer">
+        <div
+          onClick={() => goToPage('movements')}
+          className="border-2 border-black w-40 h-40 flex justify-center items-center rounded-lg cursor-pointer">
           Movimientos
         </div>
 
-        <div className="border-2 border-black w-40 h-40 flex justify-center items-center rounded-lg cursor-pointer">
+        <div
+          onClick={() => goToPage('settings')}
+          className="border-2 border-black w-40 h-40 flex justify-center items-center rounded-lg cursor-pointer">
           <p>Configuraciones</p>
         </div>
       </div>
