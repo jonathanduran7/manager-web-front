472552bc52a245272dd516629633bf15409ca45a
jonathanduran7 <joniduran624@gmail.com>
2024-01-13 10:36:27 -0300

    add modal in categories tab

--- src/app/settings/components/categories.tab.tsx
+++ src/app/settings/components/categories.tab.tsx
@@ -1,3 +1,7 @@
+'use client'
+import { Button, Dialog, DialogBody, DialogFooter, DialogHeader } from "@material-tailwind/react";
+import { useState } from "react";
+
 interface Category {
     id: number;
     name: string;
@@ -23,6 +27,35 @@ const data: Category[] = [
 ]
 
 export default function CategoriesTab() {
+    const [open, setOpen] = useState(false);
+    const [categories, setCategories] = useState<Category[]>(data);
+    const [form, setForm] = useState<Omit<Category, 'id'>>({
+        name: '',
+        type: 'income'
+    });
+    const handleOpen = () => setOpen(!open);
+
+    const handleChange = (e: any) => {
+        setForm({
+            ...form,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSave = () => {
+        setCategories([
+            ...categories,
+            {
+                id: categories.length + 1,
+                name: form.name,
+                type: form.type as 'income' | 'expense'
+            }
+        ])
+
+        setForm({ name: '', type: 'income' })
+        handleOpen();
+    }
+
     return (
         <div>
             <div className="mb-8">
@@ -33,14 +66,14 @@ export default function CategoriesTab() {
             </div>
 
             <div className="flex justify-end mb-3">
-                <button className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded" onClick={() => console.log('hola')}>
+                <button className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded" onClick={handleOpen}>
                     Add Category
                 </button>
             </div>
 
             <div>
                 {
-                    data.map((category: Category) => {
+                    categories.map((category: Category) => {
                         return (
                             <div key={category.id} className="flex justify-between items-center border-b border-gray-200 py-3">
                                 <div>
@@ -56,7 +89,39 @@ export default function CategoriesTab() {
                     })
                 }
             </div>
+            <Dialog open={open} handler={handleOpen} placeholder={""}>
+                <DialogHeader placeholder={""}>Agregar nueva cuenta</DialogHeader>
+                <DialogBody placeholder={""}>
+                    <div className="flex flex-col">
+                        <label className="text-gray-600">Name</label>
+                        <input
+                            type="text"
+                            className="border border-gray-300 rounded-sm px-4 py-2"
+                            name="name"
+                            value={form.name}
+                            onChange={handleChange}
+                        />
+                    </div>
 
+                    <div className="flex flex-col mt-4">
+                        <label className="text-gray-600">Type</label>
+                        <select className="border border-gray-300 rounded-sm px-4 py-2"
+                            name="type"
+                            value={form.type}
+                            onChange={handleChange}
+                        >
+                            <option value="income">income</option>
+                            <option value="expense">expense</option>
+                        </select>
+                    </div>
+                </DialogBody>
+                <DialogFooter placeholder={""}>
+                    <div className="flex gap-3 justify-between w-full">
+                        <button className="text-gray-600 hover:text-gray-900" onClick={handleOpen}>Cancel</button>
+                        <button className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded" onClick={handleSave}>Save</button>
+                    </div>
+                </DialogFooter>
+            </Dialog>
         </div>
     )
 }
