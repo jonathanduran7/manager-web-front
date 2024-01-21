77f6b3bbe44e71b03d86bfc413af813eff6aae35
jonathanduran7 <joniduran624@gmail.com>
2024-01-21 13:34:28 -0300

    Add grouped transactions by date in Movements page

--- src/app/movements/page.tsx
+++ src/app/movements/page.tsx
@@ -29,9 +29,59 @@ const data: Movement[] = [
         date: new Date(),
         currency: 'ARS',
         amount: 1200
-    }
+    },
+    {
+        id: '3',
+        title: 'Futbol 5',
+        account: 'Cash',
+        category: 'Sports',
+        date: new Date("2023-08-01"),
+        currency: 'ARS',
+        amount: 1200
+    },
+    {
+        id: '4',
+        title: 'Futbol 5',
+        account: 'Cash',
+        category: 'Sports',
+        date: new Date("2023-08-01"),
+        currency: 'ARS',
+        amount: 1200
+    },
+    {
+        id: '5',
+        title: 'Futbol 5',
+        account: 'Cash',
+        category: 'Sports',
+        date: new Date("2023-09-01"),
+        currency: 'ARS',
+        amount: 1200
+    },
+    {
+        id: '6',
+        title: 'Futbol 5',
+        account: 'Cash',
+        category: 'Sports',
+        date: new Date("2023-09-01"),
+        currency: 'ARS',
+        amount: 1200
+    },
 
 ]
+
+interface Result {
+    [key: string]: Movement[]
+}
+
+const registrosAgrupados = data.reduce((result: Result, registro: Movement) => {
+    const fecha = registro.date.toDateString();
+
+    if (!result[fecha]) {
+        result[fecha] = [];
+    }
+    result[fecha].push(registro);
+    return result;
+}, {});
 
 export default function Movements() {
     return (
@@ -39,7 +89,7 @@ export default function Movements() {
 
             <Header />
 
-            <div className="flex flex-col items-center justify-center">
+            <div className="flex flex-col items-center justify-center mb-10">
                 <h1 className="text-3xl font-semibold pb-2">Movements</h1>
                 <p className="text-gray-600">
                     All your transactions will be grouped by category.
@@ -49,7 +99,7 @@ export default function Movements() {
 
 
             {
-                data.length === 0 && (
+                Object.keys(registrosAgrupados).length === 0 && (
                     <div className="flex items-center justify-center">
                         <p className="text-gray-600">
                             No movements yet.
@@ -60,22 +110,28 @@ export default function Movements() {
 
             <div>
                 {
-                    data.map((movement: Movement) => {
-                        return (
-                            <div key={movement.id} className="flex justify-between items-center border-b border-gray-200 py-3">
-                                <div>
-                                    <p className="text-gray-800 font-semibold">{movement.title}</p>
-                                    <p className="text-gray-600">{movement.date.toDateString()}</p>
-                                </div>
-                                <div>
-                                    <p className="text-gray-800 font-semibold">{movement.currency} {movement.amount}</p>
-                                </div>
+                    Object.keys(registrosAgrupados).map((fecha) => (
+                        <div key={fecha} className="border-b border-gray-200">
+                            <div className="mt-4">
+                                <p className="text-gray-600">{fecha}</p>
                             </div>
-                        )
-                    })
+                            <div>
+                                {registrosAgrupados[fecha].map((registro: any) => (
+                                    <div className="flex justify-between items-center py-3">
+                                        <div>
+                                            <p className="text-gray-800 font-semibold">{registro.title}</p>
+                                            <p className="text-gray-600">{registro.account}</p>
+                                        </div>
+                                        <div>
+                                            <p className="text-gray-800 font-semibold">{registro.currency} {registro.amount}</p>
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                    ))
                 }
             </div>
-
         </main>
     )
 }
