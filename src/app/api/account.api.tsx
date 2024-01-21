6b7469279e0fbd9b688da86851924b7afd0e029d
jonathanduran7 <joniduran624@gmail.com>
2024-01-21 12:52:37 -0300

    Refactor getAccounts function to return an empty array

--- src/app/api/account.api.tsx
+++ src/app/api/account.api.tsx
@@ -3,13 +3,7 @@ import { Account } from "../settings/interfaces/account.interface";
 export const getAccounts = async () => {
     return await new Promise<Account[]>((resolve) => {
         setTimeout(() => {
-            const data: Account[] = [
-                {
-                    id: Math.random().toString(36).substr(2) + Date.now().toString(36),
-                    name: 'Cash',
-                    currency: 'ARS'
-                }
-            ]
+            const data: Account[] = []
 
             resolve(data);
         }, 1000);
