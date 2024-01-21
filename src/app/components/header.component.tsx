d40141376a68b216f22255e088b0525bcdb15ffe
jonathanduran7 <joniduran624@gmail.com>
2024-01-21 13:34:21 -0300

    modify margin bottom

--- src/app/components/header.component.tsx
+++ src/app/components/header.component.tsx
@@ -6,7 +6,7 @@ export default function Header() {
     const router = useRouter();
 
     return (
-        <div className="flex items-center mb-12">
+        <div className="flex items-center mb-4">
             <button className="flex items-center px-4 py-2 rounded-md bg-gray-100 text-gray-600" onClick={router.back}>
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-2" fill="none"
                     viewBox="0 0 24 24" stroke="currentColor">
